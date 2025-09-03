54ab62c83b0e2b02567d352205cb3741d07e7fcd
Peter Chen <p.chen@honeysucklehealth.com.au>
2025-09-03 13:21:35 +1000

    integrate nextadmin and prisma init step

---
+++ app/admin/[[...nextadmin]]/page.tsx
@@ -0,0 +1,24 @@
+ import { PromisePageProps } from "@premieroctet/next-admin";import { getNextAdminProps } from "@premieroctet/next-admin/appRouter";
+import { NextAdmin } from "@premieroctet/next-admin/adapters/next";
+import PageLoader from "@premieroctet/next-admin/pageLoader";
+import prisma from "../../../prisma";
+import "../../../nextAdminCss.css";
+import options from "../../../nextAdminOptions";
+
+export default async function AdminPage(props: PromisePageProps) {
+  const params = await props.params;
+  const searchParams = await props.searchParams;
+
+  const nextAdminProps = await getNextAdminProps({
+    params: params.nextadmin,
+    searchParams,
+    basePath: "/admin",
+    apiBasePath: "/api/admin",
+    prisma,
+    options
+  });
+
+  return (
+    <NextAdmin pageLoader={<PageLoader />} {...nextAdminProps} />
+  );
+}
