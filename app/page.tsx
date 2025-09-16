e87ae663fd5adcc833d4a5904b7b2440d735bf94
Peter Chen <p.chen@honeysucklehealth.com.au>
2025-09-16 18:22:02 +1000

    print out

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 
 export default async function Home() {
+  //print out env variables
+  console.log(process.env.DATABASE_URL);
+  console.log(process.env._AWS_ACCESS_KEY_ID);
+  console.log(process.env._AWS_SECRET_ACCESS_KEY);
   const packages = await prisma.package.findMany({
     where: { deletedAt: null },
     orderBy: { priceInCents: "asc" },
